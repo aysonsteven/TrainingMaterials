841c00c2b50d12dcd13a8575c4d7f48dad79ee89
Mark Steven A. Nonato <manonato@ICOMTEQ.LOCAL>
2018-06-01 15:55:44 +0800

    enhancement for slideshow

--- root/pages/home/home.component.ts
+++ root/pages/home/home.component.ts
@@ -16,25 +16,25 @@ export class HomePage{
     slider: slider = new slider();
     constructor(){
       let a = {
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+        'description': 'If you\'re visiting this page, you\'re likely here because you\'re searching for a random sentence. Sometimes a random word just isn\'t enough, and that is where the random sentence generator comes into play. ',
         'title': 'title',
         'imagePath': './../../assets/images/2.jpg',
         'uniqueId': 1
       }
       let b = {
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+        'description': 'By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.',
         'title': 'title 2',
         'imagePath': './../../assets/images/3.jpg',
         'uniqueId': 2
       }
       let c = {
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+        'description': 'For writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears.',
         'title': 'title 3',
         'imagePath': './../../assets/images/4.jpg',
         'uniqueId': 3
       }
       let d = {
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+        'description': 'There are a number of different ways a writer can use the random sentence for creativity. ',
         'title': 'title 4',
         'imagePath': './../../assets/images/5.jpg',
         'uniqueId': 4
@@ -70,4 +70,7 @@ export class HomePage{
         this.stocks.push( stockV2 )
         console.log( 'stock', this.stocks)
     }
+  onSearchChange(e){
+      console.log(e.srcElement.value)
+  }
 }
--- root/sharedComponents/slideshow/slideshow.component.ts
+++ root/sharedComponents/slideshow/slideshow.component.ts
@@ -8,11 +8,23 @@ import {slider} from "../../dto/slideshow.dto";
 })
 export class SlideShowComponent implements OnInit{
     @Input() slider: slider = new slider();
+    interval: any;
     constructor(){
     }
 
     ngOnInit(){
       console.log('slider', this.slider)
+      this.changeSlide();
 
+    }
+
+    changeSlide(){
+      this.interval = setInterval( ()=>{
+        if( this.slider.items.length > this.slider.selected )this.slider.selected += 1;
+        else this.slider.selected = 1;
+      }, 2000)
+    }
+    stop(){
+      this.interval.clearInterval();
     }
 }
